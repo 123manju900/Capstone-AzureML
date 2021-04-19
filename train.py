533a5b0a7eaf303edd7551328aaf289fbf7a6bab
123manju900 <51949018+123manju900@users.noreply.github.com>
2021-04-19 09:45:10 +0530

    Update train.py

--- train.py
+++ train.py
@@ -11,14 +11,14 @@ from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
 
-stroke = pd.read_csv('')
+stroke = pd.read_csv('https://raw.githubusercontent.com/123manju900/Capstone-AzureML/main/stroke-prediction-dataset.csv')
 
 run = Run.get_context()
 
 def clean_data(dp):
     
-    x_df = dp.drop('id')
-    x_df['bmi'] = x_df['bmi'].fillna(x_df['bmi'].median(), inplace =True)
+    dp.pop('id')
+    x_df['bmi'].fillna(x_df['bmi'].median(), inplace =True)
     x_df["Residence_type"] = x_df.Residence_type.apply(lambda s: 1 if s == "Urban" else 0)
     x_df["gender"] = x_df.gender.apply(lambda s: 1 if s == "Male" else 0)
     x_df["ever_married"] = x_df.ever_married.apply(lambda s: 1 if s == "Yes" else 0)
